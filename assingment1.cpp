a82a0c8a0bade2c920e5b33f5460f3d302fb64a4
LB88-del <luca.boucher@student.manchester.ac.uk>
2026-02-14 01:57:56 +0000

    Update assingment1.cpp
    commit 3 (hopefully final)

--- assingment1.cpp
+++ assingment1.cpp
@@ -5,80 +5,85 @@
 
 using namespace std;
 
+//function used to make sure input is positive
 int getPositiveInt(const string& prompt) {
     int value;
 
     while (true) {
         cout << prompt;
         cin >> value;
-
+        //if input is less than zero display error message and redo step
         if (cin.fail() || value <= 0) {
-            cout << "Invalid input. Please enter a positive integer." << endl;
-            cin.clear(); 
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+            cout << "Invalid input. Please enter a positive integer." << endl; 
+            cin.clear(); // clear error flag to stop infinite loop
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // ignore all of invalid input 
         } else {
-            return value; 
+            return value; // valid input
         }
     }
 }
 
 int main() {
-    char again = 'y';
+    const double eV = 1.602e-19; // 1eV in J
+    string again = "y"; //Inially again = "y" so that loop can start
 
-    while (true) {  
+    while (true) {  // main loop
         int Z, ni, nf;
 
-        Z = getPositiveInt("Give your Z value (must be a positive integer): ");
+        Z = getPositiveInt("Give your Z value (must be a positive integer): "); //Call getPositiveInt for Z value
 
-    
+        // validation loop for ni and nf, in a separate loop so you don't have to reinput Z if ni < nf
         while (true) {
-            ni = getPositiveInt("Give your ni value (must be a positive integer): ");
-            nf = getPositiveInt("Give your nf value (must be a positive integer): ");
+            ni = getPositiveInt("Give your ni value (must be a positive integer): "); //Call getPositiveInt for Z value
+            nf = getPositiveInt("Give your nf value (must be a positive integer): "); //Call getPositiveInt for Z value
 
             if (ni > nf) {
-                break;  
+                break;  // valid transition
             } else {
-                cout << "ni must be greater than nf for an emission transition. Try again.";
+                cout << "ni must be greater than nf. Try again." << endl; //ni < nf not valid, so repeats
             }
         }
 
-        double E = 13.6 * pow(Z, 2) * (1.0 / ni - 1.0 / nf);
+        double E = 13.6 * pow(Z, 2) * (1.0 / pow(nf,2)- 1.0 / pow(ni,2)); //calculation of energy in eV
 
-        char unit;
+        string unit;
 
         while (true) {
-            cout << "Do you want the answer in eV or joules? (e/J): ";
+            cout << "Do you want the answer in eV or joules? (e/J): "; //asks what unit user wants in
             cin >> unit;
 
-            if (unit == 'e' || unit == 'J') {
-                break;
+            if (unit.length() == 1 && unit == "e" || unit == "J") {
+                break; //if input is e or J then it is valid
             } else {
-                cout << "Invalid input, try again.\n";
+                cout << "Invalid input, try again." << endl; //otherwise the user is asked again
             }
         }
 
-        if (unit == 'e') {
-            cout << "Energy = " << E << " eV\n";
+        if (unit == "e") {
+            cout << "Energy = " << E << " eV" << endl; //calculation already done in eV
         } else {
-            double E_J = E * 1.602e-19;
-            cout << scientific << setprecision(3);
-            cout << "Energy = " << E_J << " J\n";
+            double E_J = E * eV; //convert from eV to J
+            cout << scientific << setprecision(3); //sets precision to 3dp
+            cout << "Energy = " << E_J << " J" << endl; //prints answer in J
         }
 
         while (true) {
-            cout << "Go again? (y/n): ";
+            cout << "Go again? (y/n): "; //asks user if they want to repeat
             cin >> again;
 
-            if (again == 'y' || again == 'n') {
-                break;
+
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // ignore all of invalid input 
+
+            if (again.length() == 1 && (again == "y" || again == "n")) { 
+                break; //'y' and 'n' are the only valid inputs. If again == y, main is repeated
             } else {
-                cout << "Invalid input, only enter 'y' or 'n'." ;
+                cout << "Invalid input, only enter 'y' or 'n'." << endl; //ignore invalid inputs and ask again
             }
         }
 
-        if (again == 'n') {
-            cout << "Programme terminated\n";
-            return 0;
+        if (again == "n") {
+            cout << "Programme terminated" << endl; //if again == n chosen terminate programme
+            return 0; //ends script
         }
     }
 }
